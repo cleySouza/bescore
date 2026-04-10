9f44fe1b8064a5ff646b0e79029010539936245b
Cleydson Souza <cleydson.e.s.luz@gmail.com>
2026-04-10 19:46:22 -0300

    refactor: split PreviewCard and Toggle into own subfolders
    - Move PreviewCard.tsx + PreviewCard.module.css → components/PreviewCard/
    - Move ToggleSwitch.tsx + ToggleSwitch.module.css → components/Toggle/
    - Update components/index.ts barrel exports

--- src/screen/CreateTournament/components/index.ts
+++ src/screen/CreateTournament/components/index.ts
@@ -1,2 +1,2 @@
-export { ToggleSwitch } from './ToggleSwitch'
-export { PreviewCard } from './PreviewCard'
+export { ToggleSwitch } from './Toggle/ToggleSwitch'
+export { PreviewCard } from './PreviewCard/PreviewCard'
