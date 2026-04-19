a28dd15c864aa26b7c961afb5ee76bd39f89c1a1
Cleydson Souza <cleydson.e.s.luz@gmail.com>
2026-04-19 18:38:28 -0300

    chore(version): derive app version from package.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import pkg from './package.json'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
@@ -8,6 +9,9 @@ export default defineConfig(({ mode }) => {
 
   return {
     plugins: [react()],
+    define: {
+      __APP_VERSION__: JSON.stringify(pkg.version),
+    },
     server: {
       host: true,
       proxy: {
