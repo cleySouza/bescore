25e902a244f6d35dbed646c7e2c2528f486965c5
Cleydson Souza <cleydson.e.s.luz@gmail.com>
2026-04-19 19:23:09 -0300

    fix: remove text

--- src/screen/Maintenance404/Maintenance404.tsx
+++ src/screen/Maintenance404/Maintenance404.tsx
@@ -1,12 +1,11 @@
 import styles from './Maintenance404.module.css'
-import logoName from '../../assets/logo_name.svg'
+import logoName from '../../assets/logo.svg'
 
 export function Maintenance404() {
   return (
     <div className={styles.container}>
       <div className={styles.card}>
         <img src={logoName} alt="BeScore" className={styles.logo} />
-        <span className={styles.code}>404</span>
         <h1 className={styles.title}>Em manutencao</h1>
         <p className={styles.message}>
           Estamos ajustando a plataforma para melhorar sua experiencia.
